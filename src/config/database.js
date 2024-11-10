8649e277b96a16c2701af6cd9a62ab09f412eeda
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-10 19:29:36 +0530

    feat:[bs-5] add .sequlizerc file for migrations

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/../../.env' });
 const process = require('process');
 
 const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, TEST_DATABASE, DB_HOST, DB_PORT, DB_DIALECT } = process.env;
@@ -10,13 +10,7 @@ module.exports = {
     database: DB_DATABASE,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: DB_DIALECT,
-    define: {
-      underscored: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
-    },
+    dialect: 'postgres',
   },
   test: {
     username: DB_USERNAME,
@@ -25,12 +19,6 @@ module.exports = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: DB_DIALECT,
-    define: {
-      underscored: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
-    },
   },
   production: {
     username: DB_USERNAME,
@@ -39,11 +27,5 @@ module.exports = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: DB_DIALECT,
-    define: {
-      underscored: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      deletedAt: 'deleted_at',
-    },
   },
 };
