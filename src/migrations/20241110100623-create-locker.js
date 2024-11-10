d5ea96724b395a2e08c7475700f9e02a17ff2ac8
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-10 19:27:35 +0530

    feat:[bs-5] add lockers and userslockers migrations and models

---
+++ src/migrations/20241110100623-create-locker.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('lockers', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('gen_random_uuid()'),
+      },
+      branch_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'branches',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      serial_no: {
+        type: Sequelize.STRING(20),
+        unique: true,
+        allowNull: false,
+      },
+      monthly_charge: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('lockers');
+  },
+};
