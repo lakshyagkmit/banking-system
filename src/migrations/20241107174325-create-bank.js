e6aab5ef1fe0567b263f7ebc5fc5ae3e14cbeb19
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-10 19:22:17 +0530

    feat:[bs-5] add banks migration and model

---
+++ src/migrations/20241107174325-create-bank.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('banks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('gen_random_uuid()'),
+      },
+      name: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('banks');
+  },
+};
