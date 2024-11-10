b65fe902e750ed2c07c91ddaad5b612fd60c2687
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-10 18:31:43 +0530

    fix:[bs-4] update eslint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,14 @@ import js from "@eslint/js";
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {ignores: ["temp.js", "src/migrations/*", "src/models/*"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   js.configs.recommended,
   {
         rules: {
-            "no-unused-vars": "error",
-            "no-undef": "error"
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
         }
-    }
+  }
 ];
