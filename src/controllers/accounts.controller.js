f7bd266cb7f321134be6fae8e0e9df128b3630f7
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-20 02:25:31 +0530

    fix:[38] function names and response handling in controller and services

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -1,57 +1,70 @@
 const accountService = require('../services/accounts.service');
 const commonHelper = require('../helpers/commonFunctions.helper');
 
-async function create(req, res) {
+async function create(req, res, next) {
   try {
     const { body, user } = req;
-    const account = await accountService.create(body, user);
-    res.status(201).json(account);
+    res.data = await accountService.create(body, user);
+    res.statusCode = 201;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function get(req, res) {
+async function index(req, res, next) {
   try {
     const { query, user } = req;
-    const accounts = await accountService.list(query, user);
-    res.status(200).json(accounts);
+    res.data = await accountService.index(query, user);
+    res.statusCode = 200;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function getById(req, res) {
+async function view(req, res, next) {
   try {
     const { params, user } = req;
-    const account = await accountService.listById(params, user);
+    const { id } = params;
+    const account = await accountService.view(id, user);
     if (!account) {
-      return res.status(404).json({ message: 'User not found' });
+      res.message = 'User not found';
+      res.statusCode = 404;
+      next();
     }
-    res.status(200).json(account);
+    res.data = account;
+    res.statusCode = 200;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function updateById(req, res) {
+async function update(req, res, next) {
   try {
     const { params, body, user } = req;
-    const account = await accountService.updateById(params, body, user);
-    res.status(200).json({ account, message: 'User updated successfully' });
+    const { id } = params;
+    res.message = 'Account updated successfully';
+    res.data = await accountService.update(id, body, user);
+    res.statusCode = 200;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function deleteById(req, res) {
+async function remove(req, res, next) {
   try {
     const { params, user } = req;
-    await accountService.deleteById(params.id, user);
-    res.status(204).send({ message: 'Account deleted successfully' });
+    const { id } = params;
+    await accountService.remove(id, user);
+    res.message = 'Account deleted successfully';
+    res.statusCode = 204;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-module.exports = { create, get, getById, updateById, deleteById };
+module.exports = { create, index, view, update, remove };
