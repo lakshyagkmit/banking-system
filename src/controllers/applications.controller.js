06c792966780b862ea506ac2a0ec2fd3efdc182f
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-20 02:45:54 +0530

    fix:[bs-39] fix function names in controller and service and add custom respinse in controller

--- src/controllers/applications.controller.js
+++ src/controllers/applications.controller.js
@@ -1,47 +1,56 @@
 const applicationService = require('../services/applications.service');
 const commonHelper = require('../helpers/commonFunctions.helper');
 
-async function requestAccount(req, res) {
+async function requestAccount(req, res, next) {
   try {
     const { body, user } = req;
-    const application = await applicationService.requestAccount(body, user);
-    res.status(201).json(application);
+    res.data = await applicationService.requestAccount(body, user);
+    res.statusCode = 201;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function requestLocker(req, res) {
+async function requestLocker(req, res, next) {
   try {
     const { body, user } = req;
-    const application = await applicationService.requestLocker(body, user);
-    res.status(201).json(application);
+    res.data = await applicationService.requestLocker(body, user);
+    res.statusCode = 201;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function get(req, res) {
+async function index(req, res, next) {
   try {
     const { query, user } = req;
-    const applications = await applicationService.list(query, user);
-    res.status(200).json(applications);
+    res.data = await applicationService.index(query, user);
+    res.statusCode = 201;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-async function getById(req, res) {
+async function view(req, res, next) {
   try {
     const { params, user } = req;
-    const application = await applicationService.listById(params, user);
+    const { id } = params;
+    const application = await applicationService.view(id, user);
     if (!application) {
-      return res.status(404).json({ message: 'Application not found' });
+      res.message = 'Application not found';
+      res.statusCode = 404;
+      next();
     }
     res.status(200).json(application);
+    res.data = application;
+    res.statusCode = 201;
+    next();
   } catch (error) {
     commonHelper.customErrorHandler(req, res, error.message, error.statusCode, error);
   }
 }
 
-module.exports = { requestAccount, requestLocker, get, getById };
+module.exports = { requestAccount, requestLocker, index, view };
