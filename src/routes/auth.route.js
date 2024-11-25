40cc530772d115a6ba944e7d400e6bd3341e41a3
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-25 10:32:25 +0530

    fix:[bs-65] update in auth route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,7 +3,6 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const multerMiddleware = require('../middlewares/multer.middleware');
 const authValidator = require('../validators/auth.validator');
 const authController = require('../controllers/auth.controller');
-const userSerializer = require('../serializers/users.serializer');
 const commonHelper = require('../helpers/commonFunctions.helper');
 
 const router = express.Router();
