9b29c2e5ccf760205fa357429f57c6e31f5eb172
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-27 23:41:49 +0530

    fix:[bs-78] update in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 const { decryptJwt } = require('../helpers/jwt.helper');
 const process = require('process');
+const commonHelper = require('../helpers/commonFunctions.helper');
 
 // checks user authorisation
 function checkAuthToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(401).json({ message: 'Access denied. Token is missing.' });
+    if (!token) {
+      return res.status(401).json({ message: 'Access denied. Token is missing.' });
+    }
+
+    const decryptedToken = decryptJwt(token);
+
+    jwt.verify(decryptedToken, process.env.JWT_SECRET, (err, user) => {
+      if (err) return res.status(403).json({ message: 'Invalid or expired token.' });
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    commonHelper.customErrorHandler(req, res, err.message, 400, err);
   }
-
-  const decryptedToken = decryptJwt(token);
-
-  jwt.verify(decryptedToken, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid or expired token.' });
-    req.user = user;
-    next();
-  });
 }
 
 // check user role
